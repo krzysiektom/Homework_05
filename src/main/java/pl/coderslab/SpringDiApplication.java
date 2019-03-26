29cb516764420461efe84c01dfc72cb94e6251f6
Krzysztof <krzysiektom@op.pl>
2019-03-26 11:12:06 +0100

    Zadanie 1

---
+++ src/main/java/pl/coderslab/SpringDiApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SpringDiApplication {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        System.out.println("test");
+        context.close();
+    }
+}
